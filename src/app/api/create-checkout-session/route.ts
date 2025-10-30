2ad9751a1104c6c7b92663d169a8d7d49c79a6f2
LozlinkMNA <mark@mnatechnologies.com.au>
2025-10-30 16:41:33 +1100

    Mostly complete functionally + visually

--- src/app/api/create-checkout-session/route.ts
+++ src/app/api/create-checkout-session/route.ts
@@ -10,8 +10,8 @@ export async function POST(req: NextRequest) {
     }
 
     const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-      apiVersion: '2025-09-30.clover',
-    })
+      apiVersion: '2025-10-29.clover',
+      })
 
     const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = items.map((i: any) => ({
       quantity: i.quantity || 1,
