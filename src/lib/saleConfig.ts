c2d8d830c458e372187a9aed49e7ccf65e636011
LozlinkMNA <mark@mnatechnologies.com.au>
2025-12-26 23:12:29 +1100

    extend boxing day sale

--- src/lib/saleConfig.ts
+++ src/lib/saleConfig.ts
@@ -8,7 +8,7 @@ export type SaleConfig = {
 }
 
 const BOXING_DAY_START = new Date('2025-12-26T00:00:00+11:00')
-const BOXING_DAY_END = new Date('2025-12-27T00:00:00+11:00')
+const BOXING_DAY_END = new Date('2025-12-28T00:00:00+11:00')
 
 const SALE_CONFIG = {
     discountPercent: 50,
